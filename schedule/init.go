4214d61e1da7a2f71772a56ec8e6d399caebb626
B88000005 <s444488888888@gmail.com>
2016-02-13 07:22:35 +0800

    NoAutoAdd
    Remove auto add problem from vjudge

--- schedule/init.go
+++ schedule/init.go
@@ -17,26 +17,6 @@ var ROJs = []RemoteOJInterface{&PKUJudger{}}
 
 func init() {
 	go func() {
-		for _, oj := range ROJs {
-			oj.Init()
-		}
-		for {
-			errCnt := 0
-			for _, oj := range ROJs {
-			again:
-				if err := oj.GetProblems(); err != nil {
-					errCnt++
-					if errCnt > 5 {
-						continue
-					}
-					time.Sleep(10 * time.Second)
-					goto again
-				}
-			}
-			time.Sleep(7 * 24 * time.Hour) //update per week
-		}
-	}()
-	go func() {
 		ojModel := &model.OJModel{}
 		status := &model.OJStatus{}
 		for {
